db3aa9fcd3bfc4e3c504a4f08cd13cb30965eb69
Miguel Aracena Velazquez <maav501@its.york.ac.uk>
2023-03-29 14:05:51 +0100

    After unit tests

--- tests/testthat/test-matches.R
+++ tests/testthat/test-matches.R
@@ -1,3 +1,24 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+
+test_that("uss_make_matches works", {
+
+  # use the function
+  italy <- uss_make_matches(engsoccerdata::italy, "Italy")
+
+  expect_true(tibble::is_tibble(italy))
+  expect_identical(unique(italy$country),"Italy")
+  expect_s3_class(italy$tier, "factor")
+  expect_named(
+    italy,
+    c("country", "tier", "season", "date", "home", "visitor",
+      "goals_home", "goals_visitor")
+  )
+
+
+})
+
+
+
